09a6ccace7d7c8a434d2df98dea26b1761fa6087
Andreas Pazureck <pazureck@c-s-g.net>
2017-04-05 16:42:35 +0200

    Uebung 7.1.1

---
+++ Uebungen/Kapitel7/Uebung11/Uebung1.1.c
@@ -0,0 +1,32 @@
+/**********************************************************************\
+* Kurzbeschreibung: Uebung: 7.1.1; Punkte: 0
+*
+* Datum:       Autor:               Grund der Aenderung:
+* 29.03.2017   Andreas Pazureck     Neuerstellung
+\**********************************************************************/
+#include <stdio.h>
+#include "vumrech.h"
+
+/*--- Funktionsdefinitionen ------------------------------------------*/
+int main(void)
+{
+    double distance, timeins, mps;
+
+    printf("Gib die Strecke ein (in Meter): ");
+    scanf("%lf", &distance);
+
+    printf("Gib die Zeit ein, die dafür benötigt wird (in Sekunden): ");
+    scanf("%lf", &timeins);
+
+    mps = distance / timeins;
+
+    printf("Das entspricht folgender Geschwindigkeit:\n");
+    printf("  %lf m/sec =\n", mps);
+    printf("    %lf km/h =\n", mps * MS_NACH_KMH);
+    printf("      %lf m/d =\n", mps * MS_NACH_MTAG);
+    printf("        %lf km/d", mps * MS_NACH_KMTAG);
+
+    fflush(stdin);
+    getchar();
+    return 0;
+}
---
+++ Uebungen/Kapitel7/Uebung11/vumrech.h
@@ -0,0 +1,3 @@
+#define MS_NACH_KMH     3.6
+#define MS_NACH_KMTAG   3.6*24
+#define MS_NACH_MTAG    3.6*24*1000
